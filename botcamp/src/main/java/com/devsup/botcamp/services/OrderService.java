545d603de4dfd268dd41a23a8854d785a2c15c0d
JanuarioAznick <januario-jose@outlook.com>
2022-08-21 00:29:04 +0200

    GET/orders

---
+++ botcamp/src/main/java/com/devsup/botcamp/services/OrderService.java
@@ -0,0 +1,27 @@
+package com.devsup.botcamp.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsup.botcamp.dto.OrderDTO;
+import com.devsup.botcamp.entities.Order;
+import com.devsup.botcamp.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<OrderDTO> findAll() {
+		List<Order> list = repository.findOrdersWithProducts();
+		
+		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+		
+	}
+}
